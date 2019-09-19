2443822dc55ea0246b68e64a2fa823a2e64494ed
leopheard <leopheard.misc@gmail.com>
2019-09-18 20:39:32 -0400

    upload

---
+++ README.md
@@ -0,0 +1,12 @@
+<a href="https://www.motherjones.com/podcasts/">Mother Jones</a> podcasts.<br>
+
+<a href="kodi.tv">KODI<a> / XBMC / Firestick addon for the <a href="https://www.motherjones.com/podcasts/">Mother Jones</a> podcasts.<br>
+
+Each episode will go deep on a big story you’ll definitely want to hear more about. We’ll share with you our best investigations (think private prisons, electoral skullduggery, Dark Money, and Trump's Russia connections), and informative interviews with our reporters and newsmakers. We're hoping to make your week more informed with the stories that really matter, told by us, the folks you trust for smart, fearless, reporting.<br>
+
+Also included: <a href="https://www.motherjones.com/podcasts/">Bite</a> podcast.<br>
+
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+
+<img src="https://www.motherjones.com/wp-content/themes/motherjones/img/the-mother-jones-podcast-logo-615-615.png">
+<br><a href="http://www.kodi.tv"><img src="https://kodi.tv/sites/default/files/page/field_image/about--devices.jpg">
---
+++ addon.py
@@ -0,0 +1,57 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import mainaddon
+
+plugin = Plugin()
+url1 = ""
+@plugin.route('/')
+def main_menu():
+    items = [
+        {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('episodes1'),
+            'thumbnail': "https://is3-ssl.mzstatic.com/image/thumb/Podcasts113/v4/e4/c4/ad/e4c4ad13-6cf5-1c40-5f3d-aba90668db18/mza_3523615710896971529.jpeg/600x600bb.jpg"},
+        {
+            'label': plugin.get_string(30000),
+            'path': plugin.url_for('episodes'),
+            'thumbnail': "https://is3-ssl.mzstatic.com/image/thumb/Podcasts113/v4/e4/c4/ad/e4c4ad13-6cf5-1c40-5f3d-aba90668db18/mza_3523615710896971529.jpeg/600x600bb.jpg"},
+        {
+            'label': plugin.get_string(30002),
+            'path': plugin.url_for('episodes2'),
+            'thumbnail': "https://www.motherjones.com/wp-content/themes/motherjones/img/bite-logo-615-615.png"},
+        {
+            'label': plugin.get_string(30003),
+            'path': plugin.url_for('episodes3'),
+            'thumbnail': "https://www.motherjones.com/wp-content/themes/motherjones/img/bite-logo-615-615.png"},
+    ]
+    return items
+
+@plugin.route('/episodes1/')
+def episodes1():
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
+    items = mainaddon.compile_playable_podcast1(playable_podcast1)
+    return items
+
+@plugin.route('/episodes/')
+def episodes():
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast = mainaddon.get_playable_podcast(soup1)
+    items = mainaddon.compile_playable_podcast(playable_podcast)
+    return items
+
+@plugin.route('/episodes2/')
+def episodes2():
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast2(playable_podcast2)
+    return items
+
+@plugin.route('/episodes3/')
+def episodes3():
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast3 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast3(playable_podcast3)
+    return items
+
+if __name__ == '__main__':
+    plugin.run()
